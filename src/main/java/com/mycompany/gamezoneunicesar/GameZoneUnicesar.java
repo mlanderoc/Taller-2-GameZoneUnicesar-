01896cc35b2d3392b17d422287719797c7a452b5
Merlin Landero <mlandero@unicesar.edu.co>
2026-09-02 09:05:17 -0500

    feat: Initialize the GamezoneUnicesar Maven project

---
+++ src/main/java/com/mycompany/gamezoneunicesar/GameZoneUnicesar.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.gamezoneunicesar;
+
+/**
+ *
+ * @author 57300
+ */
+public class GameZoneUnicesar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
