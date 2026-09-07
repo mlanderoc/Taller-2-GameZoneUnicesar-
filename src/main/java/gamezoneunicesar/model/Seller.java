ccc071e99798507ee03f0bfd070d6f212a9120cc
hector <hjguevara@unicesar.edu.co>
2026-09-07 08:49:20 -0500

    Implement Seller subclass with its specific attributes.

---
+++ src/main/java/gamezoneunicesar/model/Seller.java
@@ -0,0 +1,33 @@
+
+package gamezoneunicesar.model;
+
+
+public class Seller extends Person{
+    private long employeecode;
+    private String shift;
+
+    public Seller() {
+    }
+
+    public Seller(long employeecode, String shift) {
+        this.employeecode = employeecode;
+        this.shift = shift;
+    }
+
+    public long getEmployeecode() {
+        return employeecode;
+    }
+
+    public void setEmployeecode(long employeecode) {
+        this.employeecode = employeecode;
+    }
+
+    public String getShift() {
+        return shift;
+    }
+
+    public void setShift(String shift) {
+        this.shift = shift;
+    }
+    
+}
