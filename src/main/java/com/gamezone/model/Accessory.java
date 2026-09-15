584692a08c7973b2ffc001f0418125e31e510752
Carlos Gomez <craulgomez@unicesar.edu.co>
2026-09-15 13:01:14 -0500

    feat: accessory class update

--- src/main/java/com/gamezone/model/Accessory.java
+++ src/main/java/com/gamezone/model/Accessory.java
@@ -7,8 +7,26 @@ public abstract class Accessory extends Product {
     
     private List<String> compatibleConsoleIds;
 
-    public Accessory(String id, String title, double price, int stock) {
+    public Accessory(List<String> compatibleConsoleIds, String id, String title, double price, int stock) {
         super(id, title, price, stock);
+        this.compatibleConsoleIds = compatibleConsoleIds;
     }
+
+   
+
+    public List<String> getCompatibleConsoleIds() {
+        return compatibleConsoleIds;
+    }
+
+    public void setCompatibleConsoleIds(List<String> compatibleConsoleIds) {
+        this.compatibleConsoleIds = compatibleConsoleIds;
+    }
+    
+    @Override
+    public String getDescription(){
+        return "Title: " + getTitle()
+        + ", Compatible console: " + compatibleConsoleIds;
+    }
+    
     
 }
